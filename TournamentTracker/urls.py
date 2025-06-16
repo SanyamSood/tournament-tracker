c4170a81609324ceb65686b93f9271545ee9bcf6
sood <soodsanyam007007@gmail.com>
2025-06-16 18:42:41 +0530

    changed base url

--- TournamentTracker/urls.py
+++ TournamentTracker/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', select_sport, name='select_sport'), 
+    path('', include('badminton.urls')),  # Redirect to badminton app
     path('badminton/', include('badminton.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
