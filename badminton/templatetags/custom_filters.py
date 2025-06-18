b39244447f1fa42c984103431cd9f42628df61f4
sood <soodsanyam007007@gmail.com>
2025-06-18 18:31:15 +0530

    UI CHANGES

--- badminton/templatetags/custom_filters.py
+++ badminton/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def get_item(form, key):
     return form[key]
+
+@register.filter
+def get_group_points(points_table, group):
+    return points_table.filter(team__group=group)
