355c6a0448bc6695f468805290b767371836e0fb
sood <soodsanyam007007@gmail.com>
2025-06-28 16:10:57 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -7,19 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TournamentTracker.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    # Auto-create a superuser if none exist
+    # ✅ Auto-create a superuser if needed
     if os.environ.get('RENDER_SUPERUSER_ONCE') == 'true':
         import django
         django.setup()
@@ -32,4 +21,9 @@ if __name__ == '__main__':
         else:
             print("⚠️ Superuser already exists")
 
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
     main()
