7a26b3b47b094b3ca0d730f89b553d7bf4018ad4
Sanyam <soodsanyam007007@gmail.com>
2025-08-27 12:16:31 +0530

    db2

--- manage.py
+++ manage.py
@@ -7,20 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TournamentTracker.settings')
-
-    # ✅ Auto-create a superuser if needed
-    if os.environ.get('RENDER_SUPERUSER_ONCE') == 'true':
-        import django
-        django.setup()
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
-            print("✅ Superuser created")
-        else:
-            print("⚠️ Superuser already exists")
-
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
